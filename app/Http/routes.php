ec4335e0956b2ac1296d07f21380c8ae938f5423
Butschster <butschster@gmail.com>
2016-02-22 01:50:39 +0300

    Removed duplicated route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,10 +26,6 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
-});
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
