20c8870d75f7fb9f456d7de1ed7e2ecf5efba676
Butschster <butschster@gmail.com>
2016-02-22 01:49:45 +0300

    Add example for multi level navigation

--- app/Admin/navigation.php
+++ app/Admin/navigation.php
@@ -6,6 +6,48 @@ return [
         'label'    => 'Dashboard',
         'icon'     => 'dashboard',
         'url'      => route('admin.dashboard'),
-        'priority' => 0,
+        'priority' => 0
+    ],
+    [
+        'name'     => 'Settings',
+        'icon'     => 'cog',
+        'children' => [
+            [
+                'name' => 'Information',
+                'url'  => route('admin.information'),
+                'icon' => 'info-circle',
+            ]
+        ]
+    ],
+    [
+        'name'     => 'Multi-Level Dropdown',
+        'icon'     => 'sitemap',
+        'children' => [
+            [
+                'name' => 'Second Level Item',
+                'url'  => route('admin.information')
+            ],
+            [
+                'name' => 'Second Level Item',
+                'url'  => route('admin.information')
+            ],
+            [
+                'name' => 'Third Level',
+                'children' => [
+                    [
+                        'name' => 'Third Level Item',
+                        'url'  => route('admin.information')
+                    ],
+                    [
+                        'name' => 'Third Level Item',
+                        'url'  => route('admin.information')
+                    ],
+                    [
+                        'name' => 'Third Level Item',
+                        'url'  => route('admin.information')
+                    ],
+                ]
+            ],
+        ]
     ]
 ];
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -4,3 +4,8 @@ Route::get('', ['as' => 'admin.dashboard', function () {
 	$content = 'Define your dashboard here.';
 	return AdminSection::view($content, 'Dashboard');
 }]);
+
+Route::get('', ['as' => 'admin.information', function () {
+	$content = 'Define your information here.';
+	return AdminSection::view($content, 'Information');
+}]);
