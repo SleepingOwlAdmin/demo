acfb48e06d93a6626af6177bf1fdc8b56c599c47
Alexey Butochnikov <butochnikov@gmail.com>
2016-02-22 17:30:40 +0300

    Add Admin link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,7 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/admin') }}">Admin</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
