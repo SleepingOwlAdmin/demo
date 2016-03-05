fdff881edb3a407b4bd82fc8d6f32b8b4caf22f8
Butschster <butschster@gmail.com>
2016-03-05 12:10:06 +0300

    Added page tree example

---
+++ app/Admin/Page.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Model\Page;
+use SleepingOwl\Admin\Model\ModelConfiguration;
+
+AdminSection::registerModel(Page::class, function (ModelConfiguration $model) {
+    $model->setTitle('Pages');
+
+    // Display
+    $model->onDisplay(function () {
+        return AdminDisplay::tree()->setValue('title');
+    });
+
+    // Create And Edit
+    $model->onCreateAndEdit(function() {
+        return AdminForm::form()->setItems([
+            AdminFormElement::text('title', 'Title')->required(),
+            AdminFormElement::ckeditor('text', 'Text')
+        ]);
+    });
+});
