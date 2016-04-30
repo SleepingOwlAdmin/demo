7b8a1d6e445426c305515e703d2ad9c90095f40d
Asvae <ontrew@gmail.com>
2016-05-01 00:29:29 +0300

    Unit tests for NamedFormElement

--- tests/Form/Element/NamedFormElementTest.php
+++ tests/Form/Element/NamedFormElementTest.php
@@ -48,16 +48,6 @@ class NamedFormElementTest extends TestCase
     }
 
 
-    public function test_sets_and_gets_help_text()
-    {
-        $this->assertNull($this->element->getDefaultValue());
-
-        $defaultValue = 'value';
-        $this->element->setDefaultValue($defaultValue);
-        $this->assertEquals($defaultValue, $this->element->getDefaultValue());
-    }
-
-
     public function test_sets_and_gets_name()
     {
         $this->assertEquals('cat[cat1][cat2]', $this->element->getName());
@@ -77,6 +67,86 @@ class NamedFormElementTest extends TestCase
         $this->assertEquals($attribute, $this->element->getAttribute());
     }
 
+
+    public function test_sets_and_gets_help_text()
+    {
+        $this->assertEmpty($this->element->getHelpText());
+
+        $helpText = 'help text';
+        $this->element->setHelpText($helpText);
+        $this->assertEquals($helpText, $this->element->getHelpText());
+    }
+
+
+    public function test_sets_and_gets_help_text_as_htmlable()
+    {
+        $this->assertEmpty($this->element->getHelpText());
+
+        $helpText = Mockery::mock(\Illuminate\Contracts\Support\Htmlable::class);
+        $helpText->shouldReceive('toHtml')->once();
+
+        $this->element->setHelpText($helpText);
+        $this->element->getHelpText();
+    }
+
+
+    public function test_sets_and_gets_readonly()
+    {
+        $this->assertFalse($this->element->isReadonly());
+        $this->element->setReadonly(true);
+        $this->assertTrue($this->element->isReadonly());
+    }
+
+
+    public function test_adds_validation_rule_without_message()
+    {
+        $this->element->addValidationRule('rule:second');
+        $expectedRule = ['cat.cat1.cat2' => ['rule:second']];
+
+        $this->assertEquals($expectedRule,
+            $this->element->getValidationRules());
+    }
+
+
+    public function test_adds_validation_rule_with_message()
+    {
+        $this->element->addValidationRule('rule:second', 'message');
+
+        $expectedRule = ['cat.cat1.cat2' => ['rule:second']];
+        $this->assertEquals($expectedRule,
+            $this->element->getValidationRules());
+
+        $expectedMessage = ['cat[cat1][cat2].rule' => 'message'];
+        $this->assertEquals($expectedMessage,
+            $this->element->getValidationMessages());
+    }
+
+
+    public function test_adds_validation_messages()
+    {
+        $this->assertEmpty($this->element->getValidationMessages());
+
+        $this->element->addValidationMessage('rule', 'message');
+
+        $expectedMessage = ['cat[cat1][cat2].rule' => 'message'];
+        $this->assertEquals($expectedMessage,
+            $this->element->getValidationMessages());
+    }
+
+
+    public function test_sets_validation_messages()
+    {
+        $messages = ['rule' => 'message'];
+        $this->element->setValidationMessages($messages);
+
+        $expectedMessage = ['cat[cat1][cat2].rule' => 'message'];
+        $this->assertEquals($expectedMessage,
+            $this->element->getValidationMessages());
+    }
+
+    public function test_gets_value(){
+        dd($this->element->getValue());
+    }
 
     /*
      * TODO Add tests. 
