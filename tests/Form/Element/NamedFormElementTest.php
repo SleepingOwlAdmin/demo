e34a58abb90330a744a9b254288a4f57cafd6131
Asvae <ontrew@gmail.com>
2016-05-02 01:41:13 +0300

    Unit tests. Half done.

--- tests/Form/Element/NamedFormElementTest.php
+++ tests/Form/Element/NamedFormElementTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Mockery as m;
+
 class NamedFormElementTest extends TestCase
 {
 
@@ -10,13 +12,13 @@ class NamedFormElementTest extends TestCase
 
     private $path = 'cat.cat1.cat2';
 
-
     public function setUp()
     {
+        parent::setUp();
+
         $this->element = $this->getMockForAbstractClass(\SleepingOwl\Admin\Form\Element\NamedFormElement::class,
             [$this->path]);
     }
-
 
     public function test_sets_and_gets_label()
     {
@@ -27,7 +29,6 @@ class NamedFormElementTest extends TestCase
         $this->assertEquals($label, $this->element->getLabel());
     }
 
-
     public function test_sets_and_gets_path()
     {
         $this->assertEquals($this->path, $this->element->getPath());
@@ -36,7 +37,6 @@ class NamedFormElementTest extends TestCase
         $this->element->setPath($path);
         $this->assertEquals($path, $this->element->getPath());
     }
-
 
     public function test_sets_and_gets_default_value()
     {
@@ -47,7 +47,6 @@ class NamedFormElementTest extends TestCase
         $this->assertEquals($defaultValue, $this->element->getDefaultValue());
     }
 
-
     public function test_sets_and_gets_name()
     {
         $this->assertEquals('cat[cat1][cat2]', $this->element->getName());
@@ -56,7 +55,6 @@ class NamedFormElementTest extends TestCase
         $this->element->setName($name);
         $this->assertEquals($name, $this->element->getName());
     }
-
 
     public function test_sets_and_gets_attribute()
     {
@@ -67,7 +65,6 @@ class NamedFormElementTest extends TestCase
         $this->assertEquals($attribute, $this->element->getAttribute());
     }
 
-
     public function test_sets_and_gets_help_text()
     {
         $this->assertEmpty($this->element->getHelpText());
@@ -77,18 +74,16 @@ class NamedFormElementTest extends TestCase
         $this->assertEquals($helpText, $this->element->getHelpText());
     }
 
-
     public function test_sets_and_gets_help_text_as_htmlable()
     {
         $this->assertEmpty($this->element->getHelpText());
 
-        $helpText = Mockery::mock(\Illuminate\Contracts\Support\Htmlable::class);
+        $helpText = m::mock(\Illuminate\Contracts\Support\Htmlable::class);
         $helpText->shouldReceive('toHtml')->once();
 
         $this->element->setHelpText($helpText);
         $this->element->getHelpText();
     }
-
 
     public function test_sets_and_gets_readonly()
     {
@@ -96,7 +91,6 @@ class NamedFormElementTest extends TestCase
         $this->element->setReadonly(true);
         $this->assertTrue($this->element->isReadonly());
     }
-
 
     public function test_adds_validation_rule_without_message()
     {
@@ -106,7 +100,6 @@ class NamedFormElementTest extends TestCase
         $this->assertEquals($expectedRule,
             $this->element->getValidationRules());
     }
-
 
     public function test_adds_validation_rule_with_message()
     {
@@ -121,7 +114,6 @@ class NamedFormElementTest extends TestCase
             $this->element->getValidationMessages());
     }
 
-
     public function test_adds_validation_messages()
     {
         $this->assertEmpty($this->element->getValidationMessages());
@@ -133,7 +125,6 @@ class NamedFormElementTest extends TestCase
             $this->element->getValidationMessages());
     }
 
-
     public function test_sets_validation_messages()
     {
         $messages = ['rule' => 'message'];
@@ -144,8 +135,72 @@ class NamedFormElementTest extends TestCase
             $this->element->getValidationMessages());
     }
 
-    public function test_gets_value(){
-        dd($this->element->getValue());
+    /**
+     * @expectedException LogicException
+     */
+    public function test_gets_value()
+    {
+        $request = m::mock(\Illuminate\Http\Request::class);
+        $request->shouldReceive('all')->once()->andReturn(['input' => 'value']);
+        $request->shouldReceive('old')->once()->andReturn(null);
+        $request->shouldReceive('setUserResolver');
+        Request::swap($request);
+
+        $model = m::mock(\Illuminate\Database\Eloquent\Model::class);
+        $model->shouldReceive('getAttribute')
+              ->set('cat.cat1.cat2', 'test_value')
+              ->once()
+              ->andReturn('test_attribute');
+        // Required for magic methods to function.
+        $model->shouldReceive('setAttribute');
+
+        $this->element->setModel($model);
+
+        $this->markTestIncomplete('Method requires refactoring.');
+
+        $this->element->getValue();
+    }
+
+    public function test_gets_validation_rules()
+    {
+        $this->element->addValidationRule('rule');
+
+        $this->assertEquals([$this->path => ['rule']],
+            $this->element->getValidationRules());
+    }
+
+    public function test_gets_validation_rules_with_unique()
+    {
+        $this->element->addValidationRule('_unique');
+
+        $model = m::mock(\Illuminate\Database\Eloquent\Model::class);
+        $model->shouldReceive('getTable')->once()->andReturn('test_table');
+        $model->shouldReceive('exists')->andReturn(true);
+        $model->shouldReceive('getKey')->andReturn('test_table_id');
+        $this->element->setModel($model);
+
+        $rulesExpected = [
+            $this->path => ['unique:test_table,cat2,test_table_id'],
+        ];
+
+        $this->assertEquals($rulesExpected,
+            $this->element->getValidationRules());
+    }
+
+    /**
+     * @depends test_gets_value
+     */
+    public function test_is_arrayable()
+    {
+        $this->element->toArray();
+    }
+
+    /**
+     * @depends test_gets_value
+     */
+    public function test_is_savable()
+    {
+        $this->element->save();
     }
 
     /*
